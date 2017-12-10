f4069bdf6495c47c6d6f8e959082591553d4f390
alliestehney <alliestehney1@gmail.com>
2017-12-09 21:37:43 -0500

    Added tests to check pay from start to bedtime

--- src/babysitter.js
+++ src/babysitter.js
@@ -1,10 +1,22 @@
 // input hours and get paid for work
 // using a 24 hour clock
 
-export function getPaid(start, end) {
+export function checkStart(start) {
 	if (start >= 17) {
 		return true;
 	} else {
 		return false;
 	}
+}
+
+export function checkEnd(end) {
+	if (end <= 4) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+export function calculateHours(start, end, bedtime) {
+	
 }
--- src/babysitter.test.js
+++ src/babysitter.test.js
@@ -1,7 +1,37 @@
-import {getPaid} from './babysitter';
+import {checkStart} from './babysitter';
+import {checkEnd} from './babysitter';
+import {calculateHours} from './babysitter';
 
 describe("", function() {
 	it("starts no earlier than 5pm", function() {
-		expect(getPaid(17)).toEqual(true);
+		expect(checkStart(17)).toEqual(true);
+	});
+
+	it("starts no earlier than 5pm", function() {
+		expect(checkStart(13)).toEqual(false);
+	});
+
+	it("starts no earlier than 5pm", function() {
+		expect(checkStart(19)).toEqual(true);
+	});
+
+	it("ends no later than 4am", function() {
+		expect(checkEnd(5)).toEqual(false);
+	});
+
+	it("ends no later than 4am", function() {
+		expect(checkEnd(4)).toEqual(true);
+	});
+
+	it("ends no later than 4am", function() {
+		expect(checkEnd(7)).toEqual(false);
+	});
+
+	it("get paid 12$/hr from start time to bedtime", function() {
+		expect(calculateHours(18, 2, 20)).toEqual(24);
+	});
+
+	it("get paid 12$/hr from start time to bedtime", function() {
+		expect(calculateHours(18, 2, 22)).toEqual(48);
 	});
 });
